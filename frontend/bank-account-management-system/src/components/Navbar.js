65710a842cf1a0a71b663e3504b273f939b7b7aa
mattalakshmikavya <mattalakshmikavya@gmail.com>
2026-05-31 16:40:03 +0530

    build react frontend

---
+++ frontend/bank-account-management-system/src/components/Navbar.js
@@ -0,0 +1,5 @@
+function Navbar() {
+  return <h3>Navbar Component</h3>;
+}
+
+export default Navbar;
---
+++ frontend/bank-account-management-system/src/pages/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Bank Account Management System</h1>
+      <h2>Home Page</h2>
+    </div>
+  );
+}
+
+export default Home;
