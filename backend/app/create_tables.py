fc9b0beba89c3c13f4047d9cdd2bae55174b1ff2
mattalakshmikavya <mattalakshmikavya@gmail.com>
2026-06-08 20:43:02 +0530

    Move table creation logic and update main configuration

--- backend/app/create_tables.py
+++
@@ -1,9 +0,0 @@
-from database import engine, Base
-
-from models.account import Account
-from models.transaction import Transaction
-from models.system_log import SystemLog
-
-Base.metadata.create_all(bind=engine)
-
-print("All tables created successfully!")
