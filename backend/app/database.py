d405db70056df3a3fd7a0d17ceac0f1de1f491dc
mattalakshmikavya <mattalakshmikavya@gmail.com>
2026-06-09 13:00:45 +0530

    Minor updates

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
+from  env_secrets import *
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/Bank_managementdb"
+DATABASE_URL = f"postgresql://{USERNAME}:{PASSWORD}@localhost:5432/Bank_managementdb"
 
-engine = create_engine(DATABASE_URL)
+try:
+    engine=create_engine(DATABASE_URL)
+    conn = engine.connect()
+    print("Database Connected Successfully")
+except Exception as e:
+    print("Database Connection Failed")
+    print(e)
 
 Base = declarative_base()
